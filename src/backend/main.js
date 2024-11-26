1bba963f451021c7ef447aff10f74e1b39248120
TSterenborg <9018467@student.zadkine.nl>
2024-11-26 21:48:53 +0100

    build: electronjs

---
+++ src/backend/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+function createWindow () {
+    let win = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        minWidth: 1200,
+        minHeight: 600,
+        webPreferences: {
+            preload: path.join(__dirname, '../frontend/preload.js')
+        }
+    })
+
+    win.setMenuBarVisibility(false)
+    win.loadFile(path.join(__dirname, '../frontend/index.html'))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
